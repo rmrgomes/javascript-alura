4fc2a2b6ce5846d1ec28c117fd5d338643bd6648
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2022-01-10 17:27:56 -0300

    att

--- introducao-javascript-inicial/introducao-javascript/js/script.js
+++ introducao-javascript-inicial/introducao-javascript/js/script.js
@@ -6,7 +6,29 @@ var paciente = document.querySelector('#primeiro-paciente');
 var tdPeso = paciente.querySelector('.info-peso');
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector('info-altura');
+var tdAltura = paciente.querySelector('.info-altura');
 var altura = tdAltura.textContent;
 
-var imc = tdPeso / (altura * altura);
+var tdImc = paciente.querySelector('.info-imc');
+
+var pesoEhValido = true
+var alturaEhValida = true
+
+
+if (peso <= 0 || peso >= 200) {
+    tdImc.textContent = 'Peso inválido!'
+    pesoEhValido = false
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    tdImc.textContent = 'Altura inválida!'
+    alturaEhValida = false
+}
+
+if ( alturaEhValida && pesoEhValido) {
+
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
