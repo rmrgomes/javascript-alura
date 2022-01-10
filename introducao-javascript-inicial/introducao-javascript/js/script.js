61f95dd8467b0abb6249b137d1653c25c71b2d0d
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2022-01-10 16:55:12 -0300

    att

--- introducao-javascript-inicial/introducao-javascript/js/script.js
+++ introducao-javascript-inicial/introducao-javascript/js/script.js
@@ -1,2 +1,12 @@
 var title = document.querySelector('.title');
 title.textContent = 'Aparecida Nutricionista';
+
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('info-altura');
+var altura = tdAltura.textContent;
+
+var imc = tdPeso / (altura * altura);
