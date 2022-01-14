2f28480dc2dd5f9238b490c92a57a54480d6f3cf
Romário Gomes <95441690+rmrgomes@users.noreply.github.com>
2022-01-14 15:41:52 -0300

    att

--- introducao-javascript-inicial/introducao-javascript/js/form.js
+++ introducao-javascript-inicial/introducao-javascript/js/form.js
@@ -20,13 +20,17 @@ botaoAdicionar.addEventListener('click', function(event) {
 
     var tabela = document.querySelector('#tabela-pacientes');
 
-    tabela.appendChild(pacienteTr)
+    tabela.appendChild(pacienteTr);
 
-    form.reset()
+    form.reset();
+    var mensagensErro = document.querySelector('#mensagens-erro');
+    mensagensErro.innerHTML = '';
 })
 
 function exibeMensagensDeErro(erros) {
     var ul = document.querySelector('#mensagens-erro');
+    ul.innerHTML = '';
+
     erros.forEach(function(erro){
         var li =document.createElement('li');
         li.textContent = erro;
@@ -73,8 +77,25 @@ function validaPaciente(paciente) {
 
     var erros = [];
 
+    if (paciente.nome.length == 0) {
+        erros.push('O nome não pode ser em branco');
+    }
+
     if(!validaPeso(paciente.peso)) erros.push('Peso é inválido!');
     if (!validaAltura(paciente.altura)) erros.push('Altura é inválida!');
 
+    if (paciente.gordura.length == 0) {
+        erros.push('A gordura não pode ser em branco');
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push('O peso não pode ser em branco')
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push('A altura não pode ser em branco')
+    }
+
     return erros;
+    
 }
